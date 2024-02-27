5c570b35709c9fd110920586afc112e75a2f0b10
Noah Layton <116219668+RyseAbove@users.noreply.github.com>
2024-02-27 22:47:44 +0000

    Tue, Feb 27, 2024, 2:47 PM -08:00

--- css-and-js/loading.js
+++ css-and-js/loading.js
@@ -1,7 +1,24 @@
 loadText = document.querySelector('.loadingText')
 
+bg = document.querySelector('.background')
+
 load = 0
 
-loadText.innerText = `${load}%`
+int = setInterval(blurring, 30)
 
-loadText.style.opacity = 0.7
+blurring()
+
+function blurring() {
+
+    if (load > 98) {
+        clearInterval(int)
+    }
+
+    load++
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 0.7
+
+    bg.style.filter = "blur(10px)"
+
+}
+
