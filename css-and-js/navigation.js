353d84e0dd2970533a2d6249d64a4da8f0af805c
Noah Layton <116219668+RyseAbove@users.noreply.github.com>
2024-03-08 22:51:20 +0000

    Fri, Mar 8, 2024, 2:51 PM -08:00

--- css-and-js/navigation.js
+++ css-and-js/navigation.js
@@ -4,5 +4,9 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log(window.scrollY)
+    if (window.scrollY >= 200) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
 }
