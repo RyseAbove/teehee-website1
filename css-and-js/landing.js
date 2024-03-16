8550fd892bf355537a4401e90aa58456ae6238de
Noah Layton <116219668+RyseAbove@users.noreply.github.com>
2024-03-16 01:14:44 +0000

    Fri, Mar 15, 2024, 6:14 PM -07:00

--- css-and-js/landing.js
+++ css-and-js/landing.js
@@ -3,14 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
+// toggle nav open/close
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+// show modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+// close modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+// close modal on outside click
+window.addEventListener('click', () => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
